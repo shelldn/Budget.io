c18ab27ac0670a1f199b7c6b949e077115dd3661
shelldn <yevgeny.shirin@gmail.com>
2016-09-21 12:44:00 +0300

    /api/budgets done.

---
+++ src/Budget.Api/Controllers/BudgetsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Budget.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace Budget.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BudgetsController : Controller
+    {
+        //
+        // GET: /api/budgets
+
+        [HttpGet]
+        public IEnumerable<BudgetSummary> GetAll()
+        {
+            var pack = new ConventionPack { new CamelCaseElementNameConvention() };
+
+            ConventionRegistry.Register("CamelCase", pack, t => true);
+
+            var client = new MongoClient("mongodb://shelldn-ubuntu.westeurope.cloudapp.azure.com:27027/");
+            var db = client.GetDatabase("budgetio");
+            var budgets = db.GetCollection<BudgetSummary>("budgets");
+
+            return budgets
+                .Find(new BsonDocument())
+                .ToList();
+        }
+    }
+}
---
+++ src/Budget.Api/Models/BudgetSummary.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Budget.Api.Models
+{
+    [BsonIgnoreExtraElements]
+    public class BudgetSummary
+    {
+        public int Year { get; set; }
+    }
+}
