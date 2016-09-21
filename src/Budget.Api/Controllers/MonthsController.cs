211cedab39f80c18b88085c5578b5508656eed90
shelldn <yevgeny.shirin@gmail.com>
2016-09-21 12:59:43 +0300

    /api/months done.

---
+++ src/Budget.Api/Controllers/MonthsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class MonthsController : Controller
+    {
+        [HttpGet]
+        public object GetAll()
+        {
+            return new
+            {
+                data = Enumerable.Range(1, 12).Select(id => new { type = "months", id })
+            };
+        }
+    }
+}
